260a41af3c3d55ffe2054a8b21d75dca304ae6f2
Teddy Economou <teddyecon@gmail.com>
2023-01-10 11:33:59 -0500

    Lab complete

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1 = 2
+const num2 = 31
+const random = 1-100
+const num3 = 16
+const num4 = 4
 
-// Write your code here
+let multiply = num1 * num2;
+Math.random();
+let mod = num3 / num4;
+let max = Math.max(5, 10, 15, 20);
